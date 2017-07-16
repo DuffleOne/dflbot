aa7572c33616f4015d256d40019367f56938a808
George Miller <git@duffleman.co.uk>
2017-07-16 04:06:14 +0100

    web-api accepts params and it works

--- src/server/methods/play.js
+++ src/server/methods/play.js
@@ -1,11 +1,9 @@
 export default async function play(ctx) {
 	const { app, input } = ctx;
 
-	await app.join({ args: 'General' });
+	await app.join({ args: input.channel });
 
 	await app.play({
-		args: 'http://s3.duffleman.co.uk.s3-eu-west-1.amazonaws.com/music/Shake%20It%20Out%20-%20Florence%20and%20the%20Machine%20%28Instrumental%29.mp3',
+		args: input.url,
 	});
-
-	return { test: true };
 }
